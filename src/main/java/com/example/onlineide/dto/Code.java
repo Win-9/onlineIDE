de9fb486cd097b2a30c92b2afd7db9ed64a23faa
Win_9 <krrr883@gmail.com>
2022-06-27 18:22:19 +0900

    use dto for parameter

---
+++ src/main/java/com/example/onlineide/dto/Code.java
@@ -0,0 +1,12 @@
+package com.example.onlineide.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class Code {
+    private String language;
+    private String code;
+}
